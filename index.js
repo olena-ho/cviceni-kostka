05dae86a97cf7b2bb5d8fae6ea36689ae31ed4b3
Olena Horovenko <elena.gorovenko@gmail.com>
2024-05-03 18:50:53 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//event('keydown') => dice.src(++) 
+// if side6 -> ++ side1 (Po šestce zobrazte opět jedničku)
+//const side index -> src=`side${index}`
+
+const body = document.querySelector('body');
+const dice = document.querySelector('.dice');
+
+const diceRoll = () => {
+  let srcValue = dice.getAttribute('src');
+  const sideIndex = Number(srcValue.slice(-5,-4));
+
+  if(sideIndex === 6) {
+    dice.src = `img/side1.svg`
+  } else {
+    dice.src = `img/side${sideIndex + 1}.svg`
+  }
+}
+body.addEventListener('keydown', diceRoll);
+
 
